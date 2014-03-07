a2ebcdbbee58bebe767c98474d90014f6d748feb
stuartstein <stuart.stein@gmail.com>
2014-03-06 22:41:25 -0800

    Passed first two tests for learn and find

--- public/trie.js
+++ public/trie.js
@@ -1,21 +1,28 @@
 Trie = function(){
   this.characters = {};
+  this.isWord = false;
 };
 
-Trie.prototype.learn = function(word, index){
-  // This function should add the given word,
-  // starting from the given index,
-  // to this Trie.
+Trie.prototype.learn = function(word){
 
-  // It will be recursive.  It will tell
-  // the correct child of this Trie to learn the word
-  // starting from a later index.
+  // Find the first character of the word
+  var firstChar = word.charAt(0);
+  
+  // Check whether the word still has letters to learn
+  if(word.length > 0){
 
-  // Consider what the learn function should do
-  // when it reaches the end of the word?
-  // A word does not necessarily end at a leaf.
-  // You must mark nodes which are the ends of words,
-  // so that the words can be reconstructed later.
+    // Has the word learned the letter yet? If not, add it to characters
+    if(!this.characters.hasOwnProperty(firstChar)){
+      this.characters[firstChar] = new Trie(); 
+    }
+
+    // The new character needs to learn the rest of the word.
+    this.characters[firstChar].learn(word.slice(1));
+  } else {
+    // when there are no letters left to learn, set isWord to true.
+    this.isWord = true; 
+  } 
+
 };
 
 Trie.prototype.getWords = function(words, currentWord){
@@ -23,9 +30,47 @@ Trie.prototype.getWords = function(words, currentWord){
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
+  if(!words){
+    words = [];
+  } 
+  if(!currentWord) { 
+    currentWord = "";
+  } 
+
+  if(this.isWord){ 
+    words.push(currentWord); 
+  }
+  
+  for(var key in this.characters){
+    if(key){
+      return this.characters[key].getWords(words, currentWord + key);
+    }
+    else{
+      return words;
+    }
+  }
 };
 
-Trie.prototype.find = function(word, index){
+Trie.prototype.find = function(word){
+
+  // Set the first character
+  var firstChar = word.charAt(0);
+
+  // see if there are more letters to check in word
+  if(word.length > 0){
+
+    // if the trie includes firstChar, then do a find on the rest of the word
+    // otherwise, the word isn't in the tree and it should return false
+    if(this.characters.hasOwnProperty(firstChar)){
+      return this.characters[firstChar].find(word.slice(1));
+    } else {
+      return false;
+    }
+  } else{
+    // if there are no more letters, return the last trie
+    return this;
+  }
+
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
 
